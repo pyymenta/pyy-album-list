0be97f663da2717494d632b8c84f4ea6cc1cc9c9
Lucas Pimenta <lucas.pimenta94@outlook.com>
2024-09-08 22:55:53 -0300

    add album list factory contract

---
+++ src/PyyAlbumListFactory.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {PyyAlbumList} from "./PyyAlbumList.sol";
+
+contract PyyAlbumListFactory {
+    address public owner;
+
+    struct AlbumList {
+        address albumListAddress;
+        address owner;
+        string name;
+        string description;
+        uint256 createAt;
+    }
+
+    AlbumList[] public albumLists;
+    mapping(address => AlbumList[]) public userAlbumLists;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function createAlbumList(
+        string memory _name,
+        string memory _description,
+        uint256 _durationDays
+    ) external {
+        PyyAlbumList newAlbumList = new PyyAlbumList(
+            msg.sender,
+            _name,
+            _description,
+            _durationDays
+        );
+        address albumListAddress = address(newAlbumList);
+
+        AlbumList memory albumList = AlbumList({
+            albumListAddress: albumListAddress,
+            owner: msg.sender,
+            name: _name,
+            description: _description, 
+            createAt: block.timestamp
+        });
+
+        albumLists.push(albumList);
+        userAlbumLists[msg.sender].push(albumList);
+    }
+}
