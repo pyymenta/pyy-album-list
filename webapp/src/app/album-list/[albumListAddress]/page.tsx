1260fa95aaacbb6a575af67e2fb164f01422fdf8
Lucas Pimenta <lucas.pimenta94@outlook.com>
2024-10-01 12:55:38 -0300

    adjust grid

--- webapp/src/app/album-list/[albumListAddress]/page.tsx
+++ webapp/src/app/album-list/[albumListAddress]/page.tsx
@@ -92,7 +92,7 @@ export default function AlbumListPage() {
       </div>
       <div>
         <p className="text-lg font-semibold mb-4">Albuns</p>
-        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-3 xl:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4">
           {isLoadingAlbums ? (
             <p>Loading...</p>
           ) : (
